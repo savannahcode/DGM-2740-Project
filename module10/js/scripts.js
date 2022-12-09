ba87a07a9ce5b93f4742054d6342c93d14544a69
savsalmon <savannah.b.hilton@gmail.com>
2022-12-08 22:46:27 -0700

    added moveOver closs for pageWrapper on hb click

---
+++ module10/js/scripts.js
@@ -0,0 +1,7 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+
+
+hb.addEventListener('click', () => {
+    pw.classList.toggle('moveOver');
+});
