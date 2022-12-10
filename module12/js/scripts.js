00d18e5ad5296662915777b913ae1f150e1d088a
savsalmon <savannah.b.hilton@gmail.com>
2022-12-09 18:59:03 -0700

    used javascript to add html hotel cards

--- module12/js/scripts.js
+++ module12/js/scripts.js
@@ -1,5 +1,5 @@
 // what is the path to the JSON file?
-const apiURL = ""
+const apiURL = "../hoteldata.json";
 
 //Go fetch it and then wait for a response.
 fetch(apiURL)
@@ -7,13 +7,18 @@ fetch(apiURL)
   .then((myList) => {
     //Once it comes back, display it to the console.
     console.log(myList);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}); //end of "then" fat arrow function
+
+    for (let i = 0; i < myList.length; i++) {
+      let myImageTag = document.createElement("img");
+      myImageTag.src = myList[i].photo;
+
+      let myCaptionTag = document.createElement("figcaption");
+      myCaptionTag.textContent = myList[i].name;
+
+      let myFigureTag = document.createElement("figure");
+      myFigureTag.appendChild(myImageTag);
+      myFigureTag.appendChild(myCaptionTag);
+
+      document.getElementById("myCards").appendChild(myFigureTag);
+    }
+  }); //end of "then" fat arrow function
