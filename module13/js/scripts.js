fb53d79d87fb3e2689776b88507dbef711a1640d
savsalmon <savannah.b.hilton@gmail.com>
2022-12-14 16:26:32 -0700

    Weather api set up sort of in js. Adjusted html

--- module13/js/scripts.js
+++ module13/js/scripts.js
@@ -1,1 +1,9 @@
-fetch("testdata.json").then((mydata) => console.log(mydata)); //end of .then
+const apiURL =
+  "//api.openweathermap.org/data/2.5/weather?id=5780026&appid=356839bb540183cc883c1c645c9007d4&units=imperial";
+// Go fetch it and wait for a response
+fetch(apiURL).then((response) =>
+  response.json().then((weatherInfo) => {
+    // Once it comes back, display it to the console
+    console.log(weatherInfo);
+  })
+); //end of .then
