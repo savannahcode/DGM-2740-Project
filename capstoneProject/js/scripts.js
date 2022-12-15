de12f4fa83b0398ee5a6d3ad6b6c39e3c4e7d991
savsalmon <savannah.b.hilton@gmail.com>
2022-12-15 03:53:36 -0700

    added swiper scripts for kid images on home page

--- capstoneProject/js/scripts.js
+++ capstoneProject/js/scripts.js
@@ -1,3 +1,19 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  speed: 2000,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // Enable Auto Play
+  autoplay: {
+    delay: 5000,
+  },
+});
+
 const hb = document.querySelector("#hamburgerBtn");
 const pw = document.querySelector("#pageWrapper");
 
